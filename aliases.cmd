7dbfd8acf4c89297aea52599d044352a5f507f19
sergiokapone <sergiy.ponomarenko@gmail.com>
2023-06-12 19:52:19 +0300

    upd aliases

--- aliases.cmd
+++ aliases.cmd
@@ -1,4 +1,7 @@
 @echo off
+
+REM This script sets up aliases (doskeys) for running Python scripts within a pipenv virtual environment.
+
 doskey prd=pipenv run python producer.py
 doskey ce=pipenv run python consumer_email.py
 doskey cs=pipenv run python consumer_sms.py
